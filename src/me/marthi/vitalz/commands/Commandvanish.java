497c6066104394b276df9937bbf62d3310d8b231
Martijn Riemers <prive@martijnriemers.com>
2013-05-12 01:10:13 +0200

    Added permissions

--- src/me/marthi/vitalz/commands/Commandvanish.java
+++ src/me/marthi/vitalz/commands/Commandvanish.java
@@ -3,6 +3,7 @@ package me.marthi.vitalz.commands;
 import me.marthi.vitalz.vitalz;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -22,17 +23,22 @@ public class Commandvanish implements CommandExecutor {
 			String label, String[] args) {
 		Player player = (Player) sender;
 		if (sender instanceof Player) {
-			if (args[0] == null) {
-				for (Player p : Bukkit.getServer().getOnlinePlayers()) {
-					p.hidePlayer(player);
-				}
-			} else {
-				Player target = Bukkit.getPlayer(args[0]);
-				if (target != null) {
-					for (Player t : Bukkit.getServer().getOnlinePlayers()) {
-						t.hidePlayer(target);
+			if (player.hasPermission("vitalz.vanish")) {
+				if (args[0] == null) {
+					for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+						p.hidePlayer(player);
+					}
+				} else {
+					Player target = Bukkit.getPlayer(args[0]);
+					if (target != null) {
+						for (Player t : Bukkit.getServer().getOnlinePlayers()) {
+							t.hidePlayer(target);
+						}
 					}
 				}
+			} else {
+				player.sendMessage(ChatColor.RED + "You do not have access to that command!");
+				player.sendMessage(ChatColor.RED + "If you believe this is incorrect, contact an operator.");
 			}
 		} else {
 			System.out
