60534950c4c5aa47171ee630675b6bee0cac5157
Martijn Riemers <prive@martijnriemers.com>
2013-05-11 19:48:50 +0200

    Started

--- src/me/marthi/vitalz/vitalz.java
+++ src/me/marthi/vitalz/vitalz.java
@@ -1,5 +1,21 @@
 package me.marthi.vitalz;
 
-public class vitalz {
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class vitalz extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		final PluginManager pm = this.getServer().getPluginManager();
+		
+		super.onEnable();
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
 
 }
