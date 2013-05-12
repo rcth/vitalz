50c16c3d65087eb454578d4c745f05a8da2af938
Martijn Riemers <prive@martijnriemers.com>
2013-05-12 15:20:44 +0200

    cleanup

--- src/me/marthi/vitalz/vitalz.java
+++ src/me/marthi/vitalz/vitalz.java
@@ -1,6 +1,5 @@
 package me.marthi.vitalz;
 
-import java.util.Arrays;
 import java.util.List;
 
 import me.marthi.vitalz.commands.Commandfly;
@@ -13,7 +12,6 @@ import me.marthi.vitalz.listeners.LoginListener;
 
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.ChatColor;
 
 public class vitalz extends JavaPlugin {
 	
@@ -21,7 +19,6 @@ public class vitalz extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		@SuppressWarnings("unused")
 		final PluginManager pm = this.getServer().getPluginManager();
 
 		System.out.println("[Vitalz] Version [" + this.getDescription().getVersion() + "] is enabled.");
