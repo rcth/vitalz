750bef62a101fbfb69c809a2ea8db34ab2eb24c2
ipsq <prive@martijnriemers.com>
2013-05-15 17:52:15 +0200

    Changed Maven

--- src/me/marthi/vitalz/commands/Commandbroadcast.java
+++ src/me/marthi/vitalz/commands/Commandbroadcast.java
@@ -15,27 +15,31 @@ public class Commandbroadcast implements CommandExecutor {
 	public Commandbroadcast(vitalz plugin) {
 		this.plugin = plugin;
 	}
-	
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-				
-		// We don't have to make a player as it's unnecessary, because if sender = console, he has all permissions.
-		
-		if(sender.hasPermission("vitalz.broadcast")) {
-		String broadcastMessage = "";
-		
-		
-		for (int i = 0; i < args.length; i++) {
-            broadcastMessage = broadcastMessage + " " + args[i];
-          }
-		
-		Bukkit.broadcastMessage(ChatColor.GOLD + "[Broadcast]" + broadcastMessage);
+
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+
+		// We don't have to make a player as it's unnecessary, because if sender
+		// = console, he has all permissions.
+
+		if (sender.hasPermission("vitalz.broadcast")) {
+			String broadcastMessage = "";
+
+			for (int i = 0; i < args.length; i++) {
+				broadcastMessage = broadcastMessage + " " + args[i];
+			}
+
+			Bukkit.broadcastMessage(ChatColor.GOLD + "[Broadcast]"
+					+ broadcastMessage);
 		}
-		
+
 		else {
-			sender.sendMessage(ChatColor.RED + "You do not have access to that command!");
-			sender.sendMessage(ChatColor.RED + "If you believe this is incorrect, contact an operator.");
+			sender.sendMessage(ChatColor.RED
+					+ "You do not have access to that command!");
+			sender.sendMessage(ChatColor.RED
+					+ "If you believe this is incorrect, contact an operator.");
 		}
-		
+
 		return true;
 	}
 }
